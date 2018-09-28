3d1c843c6df521f27627f110d910fdb451f4df0c
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-28 11:29:32 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,8 @@ class ApplicationController < Sinatra::Base
 
   post '/posts' do
     Post.create(name: params[:name], content: params[:content])
+    @posts = Post.all
+    erb :index
   end
 
   get '/posts' do
